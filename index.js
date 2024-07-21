8d5005678f98cd29721fabbba8d17422a438f7ec
Adarsh9077 <bhardwajadarsh873@gmail.com>
2024-07-21 19:28:20 +0530

    Add index.js file

---
+++ index.js
@@ -0,0 +1,20 @@
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+for (let row of matrix) {
+    for (let value of row) {
+        console.log(value);
+    }
+}
+const str = "hello";
+for (let char of str) {
+    console.log(char);
+}
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(`Iteration ${i}`);
+}
